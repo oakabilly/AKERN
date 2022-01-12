8d3a273838b4ac5cccfcd5d558b75c9db32b5b5a
Flavio N Quercia <38718606+oakabilly@users.noreply.github.com>
2022-01-12 15:33:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      data: "Loading",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://penta.conversa-dev.it/api/v1/akern?asmm=abc&token=abc")
+      .then((response) => {})
+      .catch((error) => {
+        this.data = error.response.data;
+      });
+  },
+};
+</script>
+
+<style>
+</style>
